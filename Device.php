e11821f432cfd7345856d9b7086132a1efd7143e
Mattias <Swehunter2000@hotmail.com>
2017-04-04 19:26:07 +0200

    Started data structure

--- Device.php
+++ Device.php
@@ -9,4 +9,48 @@
 class Device
 {
 
+    private $name, $imei, $applications;
+
+    function __construct($name, $imei, $applications){
+        $this->name = $name;
+        $this->imei = $imei;
+        $this->applications = $applications;
+    }
+
+    function __sleep()
+    {
+        return array("name", "imei", "applications");
+    }
+
+    function __toString()
+    {
+
+        return json_encode($this->getObject());
+    }
+
+    function getObject(){
+        $device["name"] = $this->name;
+        $device["imei"] = $this->imei;
+        $device["applications"] = $this->applications;
+        return $device;
+    }
+
+    function addApplication($application){
+        array_push($this->applications, $application);
+    }
+
+    public function getApplications()
+    {
+        return $this->applications;
+    }
+
+    public function getName()
+    {
+        return $this->name;
+    }
+
+    public function getImei()
+    {
+        return $this->imei;
+    }
 }
